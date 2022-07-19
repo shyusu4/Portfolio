04c9af229864131563c826c84cf10d8a98cd5532
unknown <shyusupova4@gmail.com>
2022-07-19 13:51:28 +0500

    Add js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+const mobileNav = document.querySelector("mobile-nav");
+const menuIcon = document.querySelector(".fa-bars");
+const closeIcon = document.querySelector(".fa-x");
+const menuItems = document.querySelectorAll(".items");
 
+menuIcon.addEventListener("click", () => {
+  mobileNav.classList.toggle("active");
+});
 
+closeIcon.addEventListener("click", () => {
+  mobileNav.classList.remove("active");
+});
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    mobileNav.classList.remove("active");
+  });
+});
