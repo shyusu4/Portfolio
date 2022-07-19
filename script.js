4cd3d7ccbee2a616bf51067e404c0b68a33b37b5
unknown <shyusupova4@gmail.com>
2022-07-19 19:08:38 +0500

    Change double to single quote

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const humburger = document.querySelector(".humburger");
-const navToggle = document.querySelector(".mobile-nav-menu");
-const link1 = document.querySelector(".link1");
-const link2 = document.querySelector(".link2");
-const link3 = document.querySelector(".link3");
+const humburger = document.querySelector('.humburger');
+const navToggle = document.querySelector('.mobile-nav-menu');
+const link1 = document.querySelector('.link1');
+const link2 = document.querySelector('.link2');
+const link3 = document.querySelector('.link3');
 
-link1.addEventListener("click", () => {
-  humburger.classList.toggle("active");
-  navToggle.classList.toggle("active");
+link1.addEventListener('click', () => {
+  humburger.classList.toggle('active');
+  navToggle.classList.toggle('active');
 });
 
-link2.addEventListener("click", () => {
-  humburger.classList.toggle("active");
-  navToggle.classList.toggle("active");
+link2.addEventListener('click', () => {
+  humburger.classList.toggle('active');
+  navToggle.classList.toggle('active');
 });
 
-link3.addEventListener("click", () => {
-  humburger.classList.toggle("active");
-  navToggle.classList.toggle("active");
+link3.addEventListener('click', () => {
+  humburger.classList.toggle('active');
+  navToggle.classList.toggle('active');
 });
 
-humburger.addEventListener("click", () => {
-  humburger.classList.toggle("active");
-  navToggle.classList.toggle("active");
+humburger.addEventListener('click', () => {
+  humburger.classList.toggle('active');
+  navToggle.classList.toggle('active');
 });
