7e473ef95cace91a53541888c6cea00d8a0b9a23
unknown <shyusupova4@gmail.com>
2022-07-20 13:10:00 +0500

    Add changes

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ document.querySelector("#open-popup").addEventListener("click", function () {
   document.querySelector(".popup").classList.add("active");
 });
 
-document
-  .querySelector(".popup .close-btn")
-  .addEventListener("click", function () {
-    document.querySelector(".popup").classList.remove("active");
-  });
+document.querySelector(".close-btn").addEventListener("click", function () {
+  document.querySelector(".popup").classList.remove("active");
+});
