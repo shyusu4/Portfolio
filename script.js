6247a390a7524c749b5f77e202fa730a3d8a3ff7
unknown <shyusupova4@gmail.com>
2022-07-20 23:58:27 +0500

    Move html to js file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const mobileNav = document.querySelector('.mobile-nav');
 const btn = document.querySelector('.btn');
 const link1 = document.querySelector('.link1');
@@ -20,23 +21,115 @@ btn.addEventListener('click', () => {
   mobileNav.classList.toggle('active');
 });
 
+const project1 = 
+  {
+    projectId: 'projectOne',
+    name: 'Multi-Post Stories',
+    description: 'Lorem Ipsum is simply dummy text of the printing and '
+    + 'typesetting industry. Lorem Ipsum has been the industry\'s '
+    + 'standard dummy text ever since the 1500s, when an unknown printer '
+    + 'took a galley of type and scrambled it to make a type specimen book. '
+    + 'It has survived not only five centuries, but also the leap into electronic '
+    + 'typesetting, remaining essent',
+    featuredImage: 'imgs/portfolio-img.svg',
+    technologies: ['html', 'ruby on rails', 'css'],
+    liveLink: 'https://shyusu4.github.io/Portfolio/',
+    sourceLink: 'https://github.com/shyusu4/Portfolio',
+  };
+
+const project2 = 
+  {
+    projectId: 'projectOne',
+    name: 'Multi-Post Stories',
+    description: 'Lorem Ipsum is simply dummy text of the printing and '
+    + 'typesetting industry. Lorem Ipsum has been the industry\'s '
+    + 'standard dummy text ever since the 1500s, when an unknown printer '
+    + 'took a galley of type and scrambled it to make a type specimen book. '
+    + 'It has survived not only five centuries, but also the leap into electronic '
+    + 'typesetting, remaining essent',
+    featuredImage: 'imgs/portfolio-img.svg',
+    technologies: ['html', 'ruby on rails', 'css'],
+    liveLink: 'https://shyusu4.github.io/Portfolio/',
+    sourceLink: 'https://github.com/shyusu4/Portfolio',
+  };
+
+const project3 = 
+  {
+    projectId: 'projectOne',
+    name: 'Multi-Post Stories',
+    description: 'Lorem Ipsum is simply dummy text of the printing and '
+    + 'typesetting industry. Lorem Ipsum has been the industry\'s '
+    + 'standard dummy text ever since the 1500s, when an unknown printer '
+    + 'took a galley of type and scrambled it to make a type specimen book. '
+    + 'It has survived not only five centuries, but also the leap into electronic '
+    + 'typesetting, remaining essent',
+    featuredImage: 'imgs/portfolio-img.svg',
+    technologies: ['html', 'ruby on rails', 'css'],
+    liveLink: 'https://shyusu4.github.io/Portfolio/',
+    sourceLink: 'https://github.com/shyusu4/Portfolio',
+  };
+
+const project4 = 
+  {
+    projectId: 'projectOne',
+    name: 'Multi-Post Stories',
+    description: 'Lorem Ipsum is simply dummy text of the printing and '
+    + 'typesetting industry. Lorem Ipsum has been the industry\'s '
+    + 'standard dummy text ever since the 1500s, when an unknown printer '
+    + 'took a galley of type and scrambled it to make a type specimen book. '
+    + 'It has survived not only five centuries, but also the leap into electronic '
+    + 'typesetting, remaining essent',
+    featuredImage: 'imgs/portfolio-img.svg',
+    technologies: ['html', 'ruby on rails', 'css'],
+    liveLink: 'https://shyusu4.github.io/Portfolio/',
+    sourceLink: 'https://github.com/shyusu4/Portfolio',
+  };
+
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
+const projects = [project1, project2, project3, project4];
 
-const projectPopup = {
-  name: ['Multi Post Stories'],
-  image: ['imgs/portfolio-img.svg'],
-  description: [
-    `Lorem Ipsum is simply dummy text of the printing and typesetting
-    industry. Lorem Ipsum has been the industry's standard dummy text
-    ever since the 1500s, when an unknown printer took a galley of type
-    and scrambled it 1960s with the releaLorem Ipsum is simply dummy
-    text of the printing and typesetting ever since the 1500s, when an
-    unknown printer took a galley of type veris lapoa todoe.`,
-  ],
-  tools: ['html', 'ruby on rails', 'css'],
-};
+const htmlCode = `<div class="modal" id="modal">
+<div class="modal-header">
+  <div class="title">Multi Post Stories</div>
+  <button data-close-button class="close-button">&times;</button>
+</div>
+<div class="modal-body">
+  <img src="imgs/portfolio-img.svg" />
+  <p>
+    Ipsum is simply dummy text of the printing and typesetting industry.
+    Lorem Ipsum has been the industry's standard dummy text ever since
+    the 1500s, when an unknown printer took a galley of type and
+    scrambled it 1960s with the releaLorem Ipsum is simply dummy text of
+    the printing and typesetting ever since the 1500s, when an unknown
+    printer took a galley of type veris lapoa todoe.
+  </p>
+  <ul class="modal-tags">
+    <li><span>html</span></li>
+    <li><span>ruby on rails</span></li>
+    <li><span>css</span></li>
+  </ul>
+  <div class="modal-button-div">
+    <a href="#" class="modal-button"
+      >See Live
+      <img
+        class="modal-icon"
+        src="imgs/circle-arrow.svg"
+        alt="circle arrow icon"
+      />
+    </a>
+  </div>
+  <div class="modal-button-div">
+    <a href="#" class="modal-button"
+      >See Source
+      <img class="modal-icon" src="imgs/github.svg" alt="github icon" />
+    </a>
+  </div>
+</div>
+</div>
+<div id="overlay"></div>`;
+document.write(htmlCode);
 
 openModalButtons.forEach((button) => {
   button.addEventListener('click', () => {
